8030abd6f92231ef41a5850f51e1fcf2c6133dc4
Allison Brito <adbrito@espol.edu.ec>
2021-05-30 21:38:10 -0500

    pcultando morgan

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import morgan from 'morgan';
+// import morgan from 'morgan';
 import mascotaRoutes from './routes/mascota.routes';
 import authRoutes from './routes/auth.routes';
 import jwt from 'jsonwebtoken';
