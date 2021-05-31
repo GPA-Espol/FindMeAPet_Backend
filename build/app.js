45f4c8574b29969e3fa0eb82e20cfe96ea1c9549
Allison Brito <adbrito@espol.edu.ec>
2021-05-30 23:04:40 -0500

    token y morganlistos

--- build/app.js
+++ build/app.js
@@ -7,18 +7,17 @@ exports["default"] = void 0;
 
 var _express = _interopRequireDefault(require("express"));
 
+var _morgan = _interopRequireDefault(require("morgan"));
+
 var _mascota = _interopRequireDefault(require("./routes/mascota.routes"));
 
 var _auth = _interopRequireDefault(require("./routes/auth.routes"));
 
-var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// import morgan from 'morgan';
 var app = (0, _express["default"])();
-app.use(_express["default"].json()); // app.use(morgan('dev'));
-
+app.use(_express["default"].json());
+app.use((0, _morgan["default"])('dev'));
 app.get('/', function (req, res) {
   res.json('welcome');
 });
--- build/controllers/auth.controllers.js
+++ build/controllers/auth.controllers.js
@@ -5,6 +5,10 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.login = void 0;
 
+var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
@@ -13,13 +17,45 @@ var pool = require('../database');
 
 var login = /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res) {
+    var registros, usuario;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            console.log(req.body.usuario, req.body.password);
+            console.log(req.body);
+            _context.next = 3;
+            return pool.query("SELECT * FROM usuario where usuario = ? and contrasena = ? and estado ='A'", [req.body.usuario, req.body.password]);
 
-          case 1:
+          case 3:
+            registros = _context.sent;
+            console.log(registros.length, registros[0].id);
+
+            if (!(registros.length !== 1)) {
+              _context.next = 8;
+              break;
+            }
+
+            res.json("Usuario no existe");
+            return _context.abrupt("return");
+
+          case 8:
+            usuario = {
+              username: req.body.nombre,
+              password: req.body.password
+            };
+
+            _jsonwebtoken["default"].sign({
+              usuario: usuario
+            }, 'secretkey', function (error, token) {
+              res.json({
+                token: token,
+                id: registros[0].id,
+                nombre: registros[0].nombre + " " + registros[0].apellido,
+                usuario: registros[0].usuario
+              });
+            });
+
+          case 10:
           case "end":
             return _context.stop();
         }
