f0064f21ec87ce9a8bfdd3a85a8399eaeb01906b
Allison Brito <adbrito@espol.edu.ec>
2021-05-30 21:29:39 -0500

    cambios en el request de login

--- build/app.js
+++ build/app.js
@@ -18,8 +18,8 @@ var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
-app.use(_express["default"].json());
-app.use((0, _morgan["default"])('dev'));
+app.use(_express["default"].json()); // app.use(morgan('dev'));
+
 app.get('/', function (req, res) {
   res.json('welcome');
 });
--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.get('/',(req,res)=>{
     res.json('welcome');
 });
