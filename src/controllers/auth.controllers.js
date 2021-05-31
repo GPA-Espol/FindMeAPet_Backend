a4d01ceb1aa6d54a8940da0852827c3fb7029dc7
Allison Brito <adbrito@espol.edu.ec>
2021-05-30 22:56:52 -0500

    token y morganlistos

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 export const login = async (req, res) => {
     console.log(req.body);
     const registros = await pool.query("SELECT * FROM usuario where usuario = ? and contrasena = ? and estado ='A'",[req.body.usuario,req.body.password]);
-    console.log(registros.length);
+    console.log(registros.length,registros[0].id);
     if(registros.length!==1){
         res.json("Usuario no existe");
         return;
@@ -13,6 +13,6 @@ export const login = async (req, res) => {
         username:req.body.nombre,password:req.body.password
     };
     jwt.sign({usuario},'secretkey',(error,token)=>{
-        res.json({token})
+        res.json({token,id:registros[0].id,nombre:registros[0].nombre +" " + registros[0].apellido, usuario:registros[0].usuario})
     });
 };
