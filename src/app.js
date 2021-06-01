d96338af3e831a096c15f8679230daa827b5ce15
Allison Brito <adbrito@espol.edu.ec>
2021-05-31 21:52:09 -0500

    cambios

--- src/app.js
+++ src/app.js
@@ -10,8 +10,14 @@ const app = express();
 app.set('port', (process.env.PORT || 3000));
 app.use(express.json());
 app.use(morgan('dev'));
-app.get('/',(req,res)=>{
-    res.json('welcome');
+// app.get('/',(req,res)=>{
+//     res.json('welcome');
+// });
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
 app.use('/mascota',mascotaRoutes);
 app.use('/auth',authRoutes);
