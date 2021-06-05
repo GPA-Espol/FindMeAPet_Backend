5041a4a43f2a0246767b405715ea55ff52a8e500
Allison Brito <adbrito@espol.edu.ec>
2021-06-04 23:05:16 -0500

    validando que usuario no existe

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -8,12 +8,13 @@ exports.login = async (req, res) => {
     const hash = crypto.createHash('sha256').update(req.body.password).digest('base64');
     console.log(hash);
     const registros = await pool.query("SELECT id_rol FROM usuario where usuario = ? and contrasena = ? and estado ='A'",[req.body.usuario,hash]);
-    const perfil = await pool.query("SELECT nombre FROM rol where id = ? and estado ='A'",[registros[0].id_rol]);
-    console.log(registros[0].id_rol,perfil);
     if(registros.length!==1){
-        res.json("Usuario no existe");
+        res.status(401).json("Usuario no existe");
         return;
     }
+    const perfil = await pool.query("SELECT nombre FROM rol where id = ? and estado ='A'",[registros[0].id_rol]);
+    console.log(registros[0].id_rol,perfil);
+    
     const usuario ={
         id:registros[0].id_rol,rol:perfil[0].nombre
     };
