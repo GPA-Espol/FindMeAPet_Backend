5c0b0bf97685f339b87655ebb706756709ac808f
eagalvez <eagalvez@espol.edu.ec>
2021-06-09 11:04:04 -0500

    cors added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
 import mascotaRoutes from './routes/mascota.routes';
+let cors = require('cors');
 
 const app = express();
 
-
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.get('/',(req,res)=>{
