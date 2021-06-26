3b582203e36af410aecb189622f43431fa19682d
eagalvez <eagalvez@espol.edu.ec>
2021-06-26 11:03:40 -0500

    PORT for Heroku changed

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const mascotaRoutes = require('./routes/mascota.routes');
 const authRoutes = require('./routes/auth.routes');
 let cors = require('cors');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 function createServer() {
   const app = express();
