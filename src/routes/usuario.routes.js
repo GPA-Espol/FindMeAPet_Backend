a97a4afbe8b0bf134f6e6c2de22dca3de7845029
AlexVelezLl <alanvele@espol.edu.ec>
2021-08-13 15:12:34 -0500

    [FIX] GET USER BY ID.........

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -9,6 +9,7 @@ const authAdmin = passport.authenticate('jwt-admin', { session: false });
 routers.post('/', authAdmin, usuarioController.createUsuario);
 routers.get('/', authAdmin, usuarioController.getUsuarios);
 routers.get('/myUser', usuarioController.getMyUser);
+routers.get('/:usuarioId', authAdmin, usuarioController.getUsuarioById);
 routers.put('/:usuarioId', authAdmin, usuarioController.updateUsuarioById);
 routers.delete('/device', auth, usuarioController.deleteUserDevice);
 routers.delete('/:usuarioId', authAdmin, usuarioController.deleteUsuarioById);
